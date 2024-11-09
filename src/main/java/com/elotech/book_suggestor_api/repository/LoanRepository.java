eb37d62bbf619f62163ff8124c70db7ae0501491
Allan Fernando Oliveira de Andrade <allan-sjp@hotmail.com>
2024-11-09 14:00:22 -0300

    feat: loan repository

---
+++ src/main/java/com/elotech/book_suggestor_api/repository/LoanRepository.java
@@ -0,0 +1,27 @@
+package com.elotech.book_suggestor_api.repository;
+
+import com.elotech.book_suggestor_api.model.Loan;
+import com.elotech.book_suggestor_api.model.enums.LoanStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+    List<Loan> findByUserIdAndStatus(Long userId, LoanStatus status);
+    List<Loan> findByBookIdAndStatus(Long bookId, LoanStatus status);
+
+    List<Loan> findAllByStatus(LoanStatus status);
+    List<Loan> findAllByUserId(Long userId);
+    List<Loan> findAllByBookId(Long bookId);
+
+    List<Loan> findAllByCategory(String category);
+
+    List<Loan> findAllUserPreferences(Long userId);
+
+    
+    List<Loan> findActiveLoanAndReturnDateIsAtMidnight();
+    // Multiple validations
+    List<Loan> findActiveLoanAndReturnDateIsAtMidnightAndStatusIsPending();
+}
