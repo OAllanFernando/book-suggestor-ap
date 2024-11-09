74789f367b3943917f4f4a6a48a79edfc9c4b1c4
Allan Fernando Oliveira de Andrade <allan-sjp@hotmail.com>
2024-11-09 17:22:08 -0300

    feat: TDD mockito first test

--- src/test/java/com/elotech/book_suggestor_api/LoanExecutionTest.java
+++ src/test/java/com/elotech/book_suggestor_api/LoanExecutionTest.java
@@ -4,59 +4,61 @@ import com.elotech.book_suggestor_api.exception.LoanException;
 import com.elotech.book_suggestor_api.model.Book;
 import com.elotech.book_suggestor_api.model.Loan;
 import com.elotech.book_suggestor_api.model.User;
+import com.elotech.book_suggestor_api.repository.LoanRepository;
 import com.elotech.book_suggestor_api.service.LoanService;
 import com.elotech.book_suggestor_api.utils.StandartResponse;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class LoanExecutionTest {
 
-    Loan loan = new Loan();
-    Book book = new Book();
-    User user = new User();
+    @Mock
+    private LoanRepository loanRepository;
 
-    @BeforeEach
-    void beforeTheTest() {
-        book.setAuthor("Allan Tester");
-        book.setCategory("Defalt Category");
-        book.setIsbn("UNIC_ISBN");
-        book.setPublicationDate(null);
-        book.setTitle("Title");
+    @InjectMocks
+    private LoanService loanService;
 
-        user.setCreatedAt(null);
+
+
+    @Test
+    void testLoanTimeCreation() throws LoanException {
+
+        User user = new User();
         user.setEmail("Email");
         user.setName("Name");
         user.setPhoneNumber("PhoneNumber");
 
-        loan.setBook(book);
+        Book book = new Book();
+        book.setAuthor("Allan Tester");
+        book.setCategory("Default Category");
+        book.setIsbn("UNIC_ISBN");
+        book.setTitle("Title");
+
+        LocalDateTime returnDate = LocalDateTime.now().toLocalDate().atStartOfDay();
+        Loan loan = new Loan();
         loan.setUser(user);
+        loan.setBook(book);
+        loan.setLoanDate(LocalDateTime.now());
+        loan.setReturnDate(returnDate);
+
+        when(loanRepository.save(any(Loan.class))).thenReturn(loan);
+
+        Loan newLoan = this.loanService.createLoan(user, book);
+
+        verify(loanRepository, times(1)).save(any(Loan.class));
+
+        assertTrue(newLoan.getReturnDate().isBefore(newLoan.getLoanDate()),
+                StandartResponse.LOAN_INCORRECT_RETURN_DATE);
     }
-
-    @AfterEach
-    void afterTheTest() {
-        book = null;
-        user = null;
-        loan = null;
-    }
-
-    @Test
-    void TestTimeCreation() {
-
-        LoanException exceptionByWrongDate = assertThrows(LoanException.class, () -> {
-            LocalDate date = LocalDate.now();
-            LocalDate dateAfter = LocalDate.now().plusDays(1);
-            loan.setLoanDate(date);
-            loan.setReturnDate(dateAfter);
-            Loan neverloan = LoanService.createLoan(loan);
-        });
-        assertEquals(StandartResponse.LOAN_INCORRECT_RETURN_DATE, exceptionByWrongDate.getMessage());
-    }
-
-
 }
