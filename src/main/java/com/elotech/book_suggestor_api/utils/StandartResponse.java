34c8c22b8b1bec20d671089c91aa5b405b3d8276
Allan Fernando Oliveira de Andrade <allan-sjp@hotmail.com>
2024-11-09 18:51:03 -0300

    feat: Static files

--- src/main/java/com/elotech/book_suggestor_api/utils/StandartResponse.java
+++ src/main/java/com/elotech/book_suggestor_api/utils/StandartResponse.java
@@ -5,15 +5,20 @@ public class StandartResponse {
     public static final String SUCCESS = "Success";
     public static final String ERROR = "Error";
 
+
     public static final String LOAN_INCORRECT_RETURN_DATE = "The return date must be before the loan date";
-
-
     public static final String LOAN_BOOK_ALREADY_LOANED = "Book already loaned";
     public static final String LOAN_BOOK_NOT_FOUND = "Book not found";
-
-
+    public static final String LOAN_NOT_FOUND = "Loan not found";
     public static final String LOAN_USER_NOT_FOUND = "User not found";
     public static final String LOAN_USER_ALREADY_LOANED = "User already loaned a book";
     public static final String LOAN_USER_NOT_LOANED = "User not loaned a book";
 
+
+    public static final String USER_EMAIL_ALREADY_EXISTS = "Email already exists";
+
+
+    public static final String BOOK_NOT_FOUND = "Book not found";
+
+    public static final String USER_NOT_FOUND = "User not found";
 }
