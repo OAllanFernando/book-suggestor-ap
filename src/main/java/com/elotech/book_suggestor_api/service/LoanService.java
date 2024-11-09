9d4063dc2a8d187258448fc8f1e8c40b0c5b256d
Allan Fernando Oliveira de Andrade <allan-sjp@hotmail.com>
2024-11-08 22:09:55 -0300

    feat: first service class

---
+++ src/main/java/com/elotech/book_suggestor_api/service/LoanService.java
@@ -0,0 +1,11 @@
+package com.elotech.book_suggestor_api.service;
+
+import com.elotech.book_suggestor_api.exception.LoanException;
+import com.elotech.book_suggestor_api.model.Loan;
+
+public class LoanService {
+    public static Loan createLoan(Loan loan) throws LoanException {
+
+            throw new LoanException("The return date must be before the loan date");
+    }
+}
