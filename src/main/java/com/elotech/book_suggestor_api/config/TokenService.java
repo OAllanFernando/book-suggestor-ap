a62725235f81c882def3857487d3713e48f7f02b
Allan Fernando Oliveira de Andrade <allan-sjp@hotmail.com>
2024-11-10 14:35:42 -0300

    fix: issuer on decodificator

--- src/main/java/com/elotech/book_suggestor_api/config/TokenService.java
+++ src/main/java/com/elotech/book_suggestor_api/config/TokenService.java
@@ -32,7 +32,7 @@ public class TokenService {
     public String validateToken(String token) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-            return JWT.require(algorithm).withIssuer().build().verify(token).getSubject();
+            return JWT.require(algorithm).withIssuer("Book Suggestor API").build().verify(token).getSubject();
         } catch (JWTVerificationException e) {
             throw new RuntimeException("Error validating token", e);
         }
