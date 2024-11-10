7693348a4d4e9b5dc7fbab2b45eb3c117d211283
Allan Fernando Oliveira de Andrade <allan-sjp@hotmail.com>
2024-11-10 14:28:17 -0300

    fix: rest exception handler

--- src/main/java/com/elotech/book_suggestor_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/elotech/book_suggestor_api/exception/GlobalExceptionHandler.java
@@ -5,8 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
